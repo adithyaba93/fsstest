cc37e776b44d033f026c2e6ee68583d051efccd3
unknown <adithyaba93@gmail.com>
2023-06-17 16:15:51 +0530

    add middlware body-parser, cors, path

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+var corsOptions = {
+    origin: "*"
+};
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 8080
 
+const node_env = process.env.NODE_ENV;
+
+
+
 // Connect to server
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`Server is running on PORT ${PORT} in ${node_env} environment`);
 })
 
 console.log("yen samachara")
